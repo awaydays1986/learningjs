434e0115225c8ca4f501bef57c3c927f0ab89fcf
Tigran <awaydays1986@gmail.com>
2020-01-07 21:21:05 +0300

    1st homework done

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 'use strict';
-console.log("Hello!");
 
-let a = 'Fuck',
-    b = ' you!';
 
-console.log(a+b);    
-let yesOrNo = confirm("Are you an idiot?");
-console.log(yesOrNo);
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+console.log(money);
+console.log(time);
+
+let appData = {
+budget:money,
+timeData:time,
+expenses: {},
+income: [],
+savings: false
+};
+
+console.log(appData);
+
+let importantName = prompt("Введите обязательную статью расходов в этом месяце");
+
+console.log(importantName);
+
+let importantCost = prompt("Во сколько обойдется?");
+console.log(importantCost);
+
+appData.expenses.importantName = importantCost;
+
+
+
+console.log(appData.expenses.importantName);
+alert("Ваш бюджет на 1 день составляет: "+appData.budget/30);
